9bb10fe30094a339e3f49e5b3c4eb0fbba35e652
Gusbe <gusbertolo@gmail.com>
2019-04-06 23:56:17 +0200

    Iteration 1 - Soldier

--- src/viking.js
+++ src/viking.js
@@ -1,6 +1,14 @@
 // Soldier
-function Soldier() {}
-
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+  this.attack = function (){
+    return this.strength;
+  }
+  this.receiveDamage = function (damageArg){
+    this.health -= damageArg;
+  }
+}
 // Viking
 function Viking() {}
 

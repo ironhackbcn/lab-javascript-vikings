42a45d1e088e19bd8755dfb4f4eecb0b0b7e7116
Eric Capdevila <eric241@hotmail.es>
2019-04-06 13:27:18 +0200

    Done the soldier constructor and part of the viking one

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,29 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+  this.attack = function (){
+   return this.strength;
+  }
+  this.receiveDamage = function(damageArg){
+    this.health -=  damageArg;
+  }
+}
 
 // Viking
-function Viking() {}
+
+
+
+function Viking(name, healthArg, strengthArg,) {
+
+Soldier.call(this, healthArg, strengthArg);
+
+this.name = name;
+}
+
+Soldier.prototype = new Viking();
+
+
 
 // Saxon
 function Saxon() {}

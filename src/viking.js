262737964bba602e109c483579dafedf28985089
Dino Tudor <dinotudor@gmail.com>
2019-04-07 01:46:58 +0200

    WIP Iteration 2 and 3 not working inheritance

--- src/viking.js
+++ src/viking.js
@@ -3,7 +3,7 @@ function Soldier(health, strength) {
   this.health = health;
   this.strength = strength;
   this.attack = function () {
-    return strength.this;
+    return this.strength;
   }
   this.receiveDamage = function (damage) {
     this.health -= damage;
@@ -11,10 +11,44 @@ function Soldier(health, strength) {
 }
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  this.name = name;
+  this.health = health;
+  this.strength = strength;
+  /* Soldier.attack = this; */
+  this.receiveDamage = function (damageViking) {
+    this.health -= this.damageViking;
+    if (this.health > 0) {
+      return `${this.name} has received ${this.damage} points of damage`;
+    } else if (this.health <= 0) {
+      return `${this.name} has died in act of combat`;
+    };
+  }
+  this.battleCry = function(){
+    return `Odin Owns You All!`
+  }
+}
+Viking.prototype = Object.create(Soldier.prototype);
+Viking.prototype.constructor = Viking;
+
+
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.receiveDamage = function (saxonDamage) {
+    this.health -= this.saxonDamage;
+    if (this.health > 0) {
+      return `A Saxon has received ${this.saxonDamage} points of damage`;
+    } else if (this.health <= 0) {
+      return `A Saxon has died in combat`
+    }
+  };
+}
+
+Saxon.prototype = Object.create(Soldier.prototype);
+Saxon.prototype.constructor = Saxon;
 
 // War
 function War() {}

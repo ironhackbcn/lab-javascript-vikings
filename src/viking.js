158ab57fc829051ee3384cae23a9b1fba4aa06b3
BenjaminWoerner <ben.woerner@gmail.com>
2019-04-07 20:50:21 +0200

    Exercise until 2nd iteration
     Pleas enter the commit message for your changes. Lines starting

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,45 @@
 // Soldier
-function Soldier() {}
+function Soldier(healthArg, strengthArg) {
+  this.health = healthArg;
+  this.strength = strengthArg;
+  this.attack = function(){
+    return this.strength;
+    // says there is a declaration exception for the attached method?
+  }
+  this.receiveDamage = function(damage){
+    this.health = this.health - damage;  
+   }
+};
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+  Soldier.call(this, health, strength);
+  this.name = name;
+   // => if it uncomment the below it throws failures 
+   //for all other tests that have previously been passed.
+
+  //this.receiveDamage = function(damage){
+   // this.health = this.health - damage;
+    //if (this.health > 0) {
+    //return this.name + "has received" damage + "points of damage";
+     //}
+    // else {
+    //  return this.name + "has died in act of combat"
+   //};
+
+   this.battleCry = function () {
+     return "Odin Owns You All!"
+   };
+  };
 
 // Saxon
-function Saxon() {}
+function Saxon(health, strength) {
+  Soldier.call(health, strength);
+
+}
+
+
 
 // War
 function War() {}
+

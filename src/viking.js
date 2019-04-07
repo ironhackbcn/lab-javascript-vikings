5b49939c6e9bfa798d4c69a370733a6dfb9fb9fc
Gusbe <gusbertolo@gmail.com>
2019-04-07 11:01:45 +0200

    Iteration 2 - Viking

--- src/viking.js
+++ src/viking.js
@@ -1,16 +1,45 @@
 // Soldier
 function Soldier(healthArg, strengthArg) {
+
   this.health = healthArg;
   this.strength = strengthArg;
-  this.attack = function (){
-    return this.strength;
-  }
-  this.receiveDamage = function (damageArg){
-    this.health -= damageArg;
-  }
 }
+
+Soldier.prototype.attack = function (){
+
+  return this.strength;
+};
+
+Soldier.prototype.receiveDamage = function (damageArg){
+
+  this.health -= damageArg;
+};
+
+
 // Viking
-function Viking() {}
+function Viking(nameArg, healthArg, strengthArg) {
+  
+  Soldier.call(this, healthArg, strengthArg);
+  this.name = nameArg;
+}
+
+Viking.prototype = Object.create(Soldier.prototype);
+
+Viking.prototype.receiveDamage = function (damageArg){
+
+  this.health -= damageArg;
+  if(this.health > 0){
+    return `${this.name} has received ${damageArg} points of damage`;
+  }
+  else{
+    return `${this.name} has died in act of combat`;
+  }
+};
+
+Viking.prototype.battleCry = function (){
+  
+  return `Odin Owns You All!`;
+};
 
 // Saxon
 function Saxon() {}

bc4c21c4a0d9990910d70e2d59d260ffed53c013
Ivy Liu <ivyliu402@gmail.com>
2019-04-08 10:01:16 +0200

    not finish

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,37 @@
 // Soldier
-function Soldier() {}
+function Soldier(health,strength) {
+    this.health = health;
+    this.strengh = strength;
+}
+
+Soldier.prototype.attack = function (){
+    return this.strengh;
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+    this.health -= damage;
+  };
 
 // Viking
-function Viking() {}
+function Viking(name, health, strength) {
+    this.name = name;
+    Soldier.call(this,health,strength);
+};
+Viking.prototype = Object.create(Solider.prototype);
+Viking.prototype.constructor = Viking;
+
+function receiveDamage (){
+    Animal.call(this, name, owner, "Whoof");
+    this.humanRelation = "love";
+  }
+  
+
+this.battleCry()
 
 // Saxon
-function Saxon() {}
+function Saxon() 
+    Soldier.call(health,strength);
+}
 
 // War
 function War() {}

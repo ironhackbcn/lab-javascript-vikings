63bbb4c055c4dba1c11565031c8bc6cc53c47922
Johan Bautista <jsbparra@gmail.com>
2019-06-16 22:39:55 +0200

    version 1.0

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,48 @@
 // Soldier
-function Soldier() {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // Viking
-function Viking() {}
+class Viking {
+  constructor(name, health, strength) {
+    this.name = name;
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+  battleCry() {}
+}
 
 // Saxon
-function Saxon() {}
+class Saxon {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+  attack() {
+    return this.strength;
+  }
+  receiveDamage(damage) {
+    this.health -= damage;
+  }
+}
 
 // War
-function War() {}
+class War {
+  constructor() {}
+}

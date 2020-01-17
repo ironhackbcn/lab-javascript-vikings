96f40d4e57de0bedeba7d3a092b3fcb87a24edec
Stefan Berk <stefan@k2ff.de#>
2020-01-17 17:32:12 +0100

    initial commit with solider and viking

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,38 @@
 // Soldier
-function Soldier() {}
+function Soldier (healthArg, strengthArg) {
+    this.health = healthArg;
+    this.strength = strengthArg
+}
+
+Soldier.prototype.attack = function() {
+    return this.strength
+}
+
+Soldier.prototype.receiveDamage = function(damage) {
+    this.health -= damage;
+}
 
 // Viking
-function Viking() {}
+function Viking(name, healthArg, strengthArg) {
+    Soldier.call(this, healthArg, strengthArg);
+    this.name = name;
+}
+
+Viking.prototype = Object.create(Soldier.prototype); 
+Viking.prototype.constructor = Viking;
+
+Viking.prototype.receiveDamage = function(damage) {
+    this.health -= damage;
+    if (this.health <= 0) {
+        return `${this.name} has died in act of combat`
+    } else {
+        return `${this.name} has received ${damage} points of damage`
+    }
+}
+
+Viking.prototype.battleCry = function (){
+    return "Odin Owns You All!"
+}
 
 // Saxon
 function Saxon() {}

2c9f60ef9345939174f671dac08e57d47d23a5dd
LolaEnBeta <lolitaenbeta@gmail.com>
2020-01-17 17:18:41 +0100

    Create reciveDamage() function for Viking

--- src/viking.js
+++ src/viking.js
@@ -14,7 +14,14 @@ function Soldier(health, strength) {
 function Viking(name, health, strength) {
   Soldier.call(this, health, strength);
   this.name = name;
-
+  this.receiveDamage = function (damage) {
+    this.health -= damage;
+    if (this.health > 0) {
+      return `${this.name} has received ${damage} points of damage`;
+    } else {
+      return `${this.name} has died in act of combat`;
+    }
+  }
 }
 
 Viking.prototype = Object.create(Soldier.prototype);
